c83bc0f61d60938e9780cc58624f1efb9546baa8
lijun <ceykmc@gmail.com>
2018-06-08 09:59:53 +0800

    add fragment error component

--- fragment_error.py
+++ fragment_error.py
@@ -10,6 +10,7 @@ Paper: On The Stability of Video Detection and Tracking.
        Section 3.2.1
 """
 
+from collections import defaultdict
 import numpy as np
 
 from utility import iou
@@ -21,7 +22,7 @@ def single_frame_match(predicts, ground_truths, predict_score_threshold, iou_thr
         two dimension numpy array, each row has format [score, x1, y1, x2, y2], shape is m x 5
         where m corresponds to predict rectangle number
     :param ground_truths: numpy array,
-        two dimension numpy array, each row has format [x1, y1, x2, y2], shape is n x 4
+        two dimension numpy array, each row has format [person_id, x1, y1, x2, y2], shape is n x 4
         where n corresponds to ground truth rectangle number
     :param predict_score_threshold:
         any predict whose score is less than \predict_score_threshold will be ignored
@@ -30,18 +31,20 @@ def single_frame_match(predicts, ground_truths, predict_score_threshold, iou_thr
         each element in the list represents if the corresponding ground_truth is matched,
         value 0 represent for unmatched, value 1 represent for matched.
     """
-    predict_scores = predicts[:, 0]
-    predict_boxes = predicts[:, 1:]
-    predict_boxes = predict_boxes[np.where(predict_scores >= predict_score_threshold)]
     m, n = predicts.shape[0], ground_truths.shape[0]
+    assert n > 0, "there must be at least one ground truth"
 
     if m == 0:  # there are no predict boxes to be matched
         return [0] * n
 
+    predict_scores = predicts[:, 0]
+    predict_boxes = predicts[:, 1:]
+    predict_boxes = predict_boxes[np.where(predict_scores >= predict_score_threshold)]
+
     predict_box_used_flag = [0] * m
     ground_truth_matched_flag = [0] * n
     for i in range(n):
-        ground_truth = ground_truths[i]
+        ground_truth = ground_truths[i][1:]  # exclude person id
         overlaps = np.array([iou(predict_box, ground_truth) for predict_box in predict_boxes])
         max_overlap = np.max(overlaps)
         max_overlap_index = np.argmax(overlaps).item()
@@ -49,6 +52,60 @@ def single_frame_match(predicts, ground_truths, predict_score_threshold, iou_thr
             predict_box_used_flag[max_overlap_index] = 1
             ground_truth_matched_flag[i] = 1
     return ground_truth_matched_flag
+
+
+def compute_fragment_error(matched_record):
+    # As a special case, we define fragment error of a trajectory with length one to be 0.
+    if len(matched_record) == 1:
+        return 0
+    f_k = 0
+    for i in range(1, len(matched_record)):
+        if matched_record[i] != matched_record[i - 1]:
+            f_k += 1
+    return f_k / (len(matched_record) - 1)
+
+
+def fragment_error(video_predicts, video_ground_truths, predict_score_threshold=0.4, iou_threshold=0.5):
+    """
+    :param video_predicts: list
+        predict result on each frame of the video, has \m elements,
+            where m equal to the frame number in the video
+        each element represents predict results on corresponding frame,
+            has format [[score, x1, y1, x2, y2], [score, x1, y1, x2, y2], ... , [score, x1, y1, x2, y2]]
+            may have zero predict result, corresponding format is []
+    :param video_ground_truths: list
+        ground truth on each frame of the video, has \m elements,
+            where m equal to the frame number in the video
+        each element represents predict results on corresponding frame,
+            has format [[person_id, x1, y1, x2, y2], [person_id, x1, y1, x2, y2], ... , [person_id, x1, y1, x2, y2]]
+            may have zero predict result, corresponding format is []
+    :param predict_score_threshold: float
+    :param iou_threshold: float
+    :return: dict
+        fragment_error for each trajectory
+    """
+    assert len(video_predicts) == len(video_ground_truths)
+
+    # step 1: get each trajectory match record
+    trajectories_matched_record = defaultdict(list)
+    m = len(video_predicts)
+    for i in range(m):
+        if len(video_ground_truths[i]) == 0:
+            continue
+        predicts = np.array(video_predicts[i])
+        ground_truths = np.array(video_ground_truths[i])
+        ground_truth_matched_flag = \
+            single_frame_match(predicts, ground_truths, predict_score_threshold, iou_threshold)
+        assert len(ground_truth_matched_flag) == ground_truths.shape[0]
+        n = len(ground_truth_matched_flag)
+        for j in range(n):
+            person_id = ground_truths[j][0]
+            trajectories_matched_record[person_id].append(ground_truth_matched_flag[j])
+    # step 2: compute each trajectory fragment error
+    trajectories_fragment_error = dict()
+    for person_id, match_record in trajectories_matched_record.items():
+        trajectories_fragment_error[person_id] = compute_fragment_error(match_record)
+    return trajectories_fragment_error
 
 
 def main():
